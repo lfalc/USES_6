8bd5846082418bd7a192256fe0d0c1dc28120776
lfalc <lorenzfalcioni@gmail.com>
2023-11-23 08:32:59 +0100

    add missing files

---
+++ Observer/Observer.hpp
@@ -0,0 +1,22 @@
+#ifndef	_HFDP_CPP_OBSERVER_OBSERVER_HPP_
+#define _HFDP_CPP_OBSERVER_OBSERVER_HPP_
+
+#include "WeatherStation.hpp"
+
+namespace HeadFirstDesignPatterns 
+{
+	namespace Observer 
+	{
+
+		class Observer 
+		{
+			protected: virtual ~Observer() = 0 
+			{
+			};
+
+			protected: virtual void update(float temp, float humidity, float pressure) = 0;
+		};
+	} // namespace Observer
+} // namespace HeadFirstDesignPatterns
+
+#endif
